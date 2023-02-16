3b6a5a7269d0a2f21fbfbc70b69a6d9488ef44cb
Josh <josh@joshuacurtis.dev>
2023-02-16 19:59:37 +0000

    loadmore

--- main.js
+++ main.js
@@ -2,19 +2,20 @@
 const userInput = document.querySelector("#userInput").value;
 
 const baseUrl = 'https://api.thecatapi.com/v1/images/search';
-const limit = '?limit=4';
+const limit = '?limit=';
 const breed = `?breed_ids=${userInput}`;
-const url = `${baseUrl}${limit}`;
+const url = `${baseUrl}`;
 
 let btn = document.querySelector('.randombtn');
 const loader = document.querySelector('#loading');
 
-async function getData() {
+
+async function getData(number) {
     try {
         // Loader for async await
         displayLoading();
 
-        const response = await fetch(url, {
+        const response = await fetch(url + limit + number, {
             headers: {
                 'X-API-KEY': 'live_lVv7VeANlKUzgSmM9zhH5D4cI84nfokyDU7eeJWdbjewBNM0y7l2G0vhc3Jey9Rn',
             },
@@ -39,7 +40,7 @@ async function renderData() {
     let imageHtml = '';
 
     // await data
-    let data = await getData();
+    let data = await getData(5);
 
     // Loop and store in imagehtml variable
     for(const value of data) {
@@ -48,15 +49,32 @@ async function renderData() {
             <div class="render-img" style="background-image: url('${value.url}')" /></div>
         </div>
         `;
-
     }
 
     document.querySelector('#images .row').innerHTML = imageHtml;
 }
 
-function loadmoreData() {
-    let loadmore = document.querySelector('.loadmore');
-}
+let loadmore = document.querySelector('.loadmore');
+loadmore.addEventListener('click', async function () {
+
+    let imageHtml = '';
+
+    // await data
+    let data = await getData(5);
+
+    // Loop and store in imagehtml variable
+    for(const value of data) {
+        imageHtml += `
+        <div class="col-6">
+            <div class="render-img" style="background-image: url('${value.url}')" /></div>
+        </div>
+        `;
+    }
+
+    document.querySelector('#images .row').innerHTML += imageHtml;
+});
+
+
 
 
 // Loading 
